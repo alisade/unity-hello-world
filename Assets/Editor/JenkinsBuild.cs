5cb77af324dec3699724bc7e231ce1ccbc2da484
administrator <administrator@9793.local>
2019-10-16 17:10:31 -0700

    add unity_cli

--- Assets/Editor/JenkinsBuild.cs
+++ Assets/Editor/JenkinsBuild.cs
@@ -11,100 +11,151 @@ using UnityEditor.Build.Reporting;
 // ------------------------------------------------------------------------
 public class JenkinsBuild {
 
-  static string[] EnabledScenes = FindEnabledEditorScenes();
+    static string[] EnabledScenes = FindEnabledEditorScenes();
 
-  // ------------------------------------------------------------------------
-  // called from Jenkins
-  // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    // called from Jenkins
+    // Asset Bundle Builder
+    // ------------------------------------------------------------------------
+    public static void BuildAndroidAssetBundles() {
+        // just initializing here, will be overwritten
+        string targetDir = "/AssetBundles/Android";
 
-  public static void BuildAndroid(){
-    // Builds both main project and assets bundles based on passed flag
-    // just initializing here, will be overwritten
-    string appName = "APP_NAME";
-    string targetDir = "TARGET_DIR";
-    // https://docs.unity3d.com/ScriptReference/BuildTarget.html
-    string buildTarget = "BUILD_TARGET";
-    bool buildAssets = false;
-    string assetBundlesOutputDir = "AssetBundles/Android";
-
-    // find: -executeMethod
-    //   +1: JenkinsBuild.BuildAndroid
-    //   +2: VRDungeons
-    //   +3: /Users/Shared/Jenkins/Home/jobs/VRDungeons/builds/47/output
-    string[] args = System.Environment.GetCommandLineArgs();
-    for (int i=0; i<args.Length; i++){
-      if (args[i] == "-executeMethod"){
-        if (i+5 < args.Length){
-          buildTarget = args[i+1];
-          appName = args[i+2];
-          targetDir = args[i+3];
-          buildAssets = bool.Parse(args[i+4]);
-          assetBundlesOutputDir = args[i+5];
-          i += 5;
+        // find: -executeMethod
+        //   +1: JenkinsBuild.BuildAndroidAssetBundles
+        //   +2: /Users/Shared/Jenkins/Home/jobs/VRDungeons/builds/47/output/Android/AssetBundles
+        string[] args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-executeMethod")
+            {
+                if (i + 2 < args.Length)
+                {
+                    //[i + 1] is method name
+                    targetDir = args[i + 2];
+                    i += 2;
+                }
+                else
+                {
+                    System.Console.WriteLine("[JenkinsBuildAssetBundles] Incorrect Parameters for -executeMethod Format: -executeMethod <build method> <output dir>");
+                    EditorApplication.Exit(1);
+                }
+            }
         }
-        else {
-          System.Console.WriteLine("[JenkinsBuild] Incorrect Parameters for -executeMethod Format: -executeMethod <build target> <app name> <output dir> <build assets bool> <AssetBundles OutputDir>");
-          EditorApplication.Exit(1);
+        BuildProjectAssetBundles(targetDir, BuildTargetGroup.Standalone, BuildAssetBundleOptions.None, BuildTarget.Android);
+    }
+    //--------------------------------------------------------------------------
+    // called from Jenkins
+    // APK Build
+    //--------------------------------------------------------------------------
+    public static void BuildAndroid(){
+        // just initializing here, will be overwritten
+        string appName = "APP_NAME";
+        string targetDir = "TARGET_DIR";
+
+        // find: -executeMethod
+        //   +1: JenkinsBuild.BuildAndroid
+        //   +2: VRDungeons
+        //   +3: /Users/Shared/Jenkins/Home/jobs/VRDungeons/builds/47/output
+        string[] args = System.Environment.GetCommandLineArgs();
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-executeMethod")
+            {
+                if (i + 3 < args.Length)
+                {
+                    // [i + 1] is method name
+                    appName = args[i + 2];
+                    targetDir = args[i + 3];
+                    i += 3;
+                }
+                else
+                {
+                    System.Console.WriteLine("[JenkinsBuild] Incorrect Parameters for -executeMethod Format: -executeMethod <build method> <app name> <output dir>");
+                    EditorApplication.Exit(1);
+                }
+            }
         }
-      }
+
+        // for now we only support android build target
+        // e.g. // /Users/Shared/Jenkins/Home/jobs/VRDungeons/builds/47/output/VRDungeons.apk
+        string fullPathAndName = targetDir + System.IO.Path.DirectorySeparatorChar + appName + ".apk";
+        BuildProject(EnabledScenes, fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.Android, BuildOptions.None);
+
     }
+    // ------------------------------------------------------------------------
+    // ------------------------------------------------------------------------
+    private static string[] FindEnabledEditorScenes()
+    {
 
-    // for now we only support android build target
-    // e.g. // /Users/Shared/Jenkins/Home/jobs/VRDungeons/builds/47/output/VRDungeons.apk
-    string fullPathAndName = targetDir + System.IO.Path.DirectorySeparatorChar + appName + ".apk";
-    BuildProject(EnabledScenes, fullPathAndName, BuildTargetGroup.Standalone, BuildTarget.Android, BuildOptions.None, buildAssets, BuildAssetBundleOptions.None, assetBundlesOutputDir);
-  }
-
-  // ------------------------------------------------------------------------
-  // ------------------------------------------------------------------------
-  private static string[] FindEnabledEditorScenes(){
-
-    List<string> EditorScenes = new List<string>();
-    foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes){
-      if (scene.enabled){
-        EditorScenes.Add(scene.path);
-      }
+        List<string> EditorScenes = new List<string>();
+        foreach (EditorBuildSettingsScene scene in EditorBuildSettings.scenes)
+        {
+            if (scene.enabled)
+            {
+                EditorScenes.Add(scene.path);
+            }
+        }
+        return EditorScenes.ToArray();
     }
-    return EditorScenes.ToArray();
-  }
+    //--------------------------------------------------------------------------
+    private static void BuildProjectAssetBundles(string targetDir, BuildTargetGroup buildTargetGroup, BuildAssetBundleOptions buildAssetBundleOptions, BuildTarget buildTarget)
+    {
+        System.Console.WriteLine("[JenkinsAssetBundlesBuild] Building:" + targetDir + ", buildTarget:" + buildTarget.ToString());
 
-  // ------------------------------------------------------------------------
-  // e.g. BuildTargetGroup.Standalone, BuildTarget.StandaloneOSX
-  // ------------------------------------------------------------------------
-  private static void BuildProject(string[] scenes, string targetDir, BuildTargetGroup buildTargetGroup, BuildTarget buildTarget,
-                                   BuildOptions buildOptions, bool buildAssets, BuildAssetBundleOptions buildAssetBundleOptions, string assetBundlesOutputDir){
-    System.Console.WriteLine("[JenkinsBuild] Building:" + targetDir + " buildTargetGroup:" + buildTargetGroup.ToString() + " buildTarget:" + buildTarget.ToString());
+        // https://docs.unity3d.com/ScriptReference/EditorUserBuildSettings.SwitchActiveBuildTarget.html
+        bool switchResult = EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
+        if (switchResult)
+        {
+            System.Console.WriteLine("[JenkinsAssetBundlesBuild] Successfully changed Build Target to: " + buildTarget.ToString());
+        }
+        else
+        {
+            System.Console.WriteLine("[JenkinsAssetBundlesBuild] Unable to change Build Target to: " + buildTarget.ToString() + " Exiting...");
+            EditorApplication.Exit(1);
+        }
+        AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(targetDir, buildAssetBundleOptions, buildTarget);
+        string[] bundle = manifest.GetAllAssetBundles();
+        if (bundle.Length != 0)
+        {
+            System.Console.WriteLine("[JenkinsAssetBundlesBuild] Assets Bundle Build Success, Build AssetBuncles Count: " + bundle.Length);
+        }
+        else
+        {
+            System.Console.WriteLine("[JenkinsAssetBundlesBuild] Assets Build Bundle Failed");
+            EditorApplication.Exit(1);
+        }
+    }
+    // ------------------------------------------------------------------------
+    // e.g. BuildTargetGroup.Standalone, BuildTarget.Android
+    // ------------------------------------------------------------------------
+    private static void BuildProject(string[] scenes, string targetDir, BuildTargetGroup buildTargetGroup, BuildTarget buildTarget, BuildOptions buildOptions)
+    {
+        System.Console.WriteLine("[JenkinsBuild] Building:" + targetDir + " buildTargetGroup:" + buildTargetGroup.ToString() + " buildTarget:" + buildTarget.ToString());
 
-    // https://docs.unity3d.com/ScriptReference/EditorUserBuildSettings.SwitchActiveBuildTarget.html
-    bool switchResult = EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
-    if (switchResult){
-      System.Console.WriteLine("[JenkinsBuild] Successfully changed Build Target to: " + buildTarget.ToString());
-    }
-    else {
-      System.Console.WriteLine("[JenkinsBuild] Unable to change Build Target to: " + buildTarget.ToString() + " Exiting...");
-      EditorApplication.Exit(1);
-    }
+        // https://docs.unity3d.com/ScriptReference/EditorUserBuildSettings.SwitchActiveBuildTarget.html
+        bool switchResult = EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
+        if (switchResult)
+        {
+            System.Console.WriteLine("[JenkinsBuild] Successfully changed Build Target to: " + buildTarget.ToString());
+        }
+        else
+        {
+            System.Console.WriteLine("[JenkinsBuild] Unable to change Build Target to: " + buildTarget.ToString() + " Exiting...");
+            EditorApplication.Exit(1);
+        }
 
-    // https://docs.unity3d.com/ScriptReference/BuildPipeline.BuildPlayer.html
-    //BuildReport buildReport = BuildPipeline.BuildPlayer(scenes, targetDir, buildTarget, buildOptions);
-    //BuildSummary buildSummary = buildReport.summary;
-    //if (buildSummary.result == BuildResult.Succeeded){
-    //  System.Console.WriteLine("[JenkinsBuild] Build Success: Time:" + buildSummary.totalTime + " Size:" + buildSummary.totalSize + " bytes");
-    //}
-    //else {
-    //  System.Console.WriteLine("[JenkinsBuild] Build Failed: Time:" + buildSummary.totalTime + " Total Errors:" + buildSummary.totalErrors);
-    //  EditorApplication.Exit(1);
-    //}
-    if (buildAssets) {
-      AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(assetBundlesOutputDir, buildAssetBundleOptions, buildTarget);
-      string[] bundle = manifest.GetAllAssetBundles();
-      if (bundle.Length != 0 ){
-        System.Console.WriteLine("[JenkinsBuild] Assets Bundle Build Success");
-      }
-      else {
-        System.Console.WriteLine("[JenkinsBuild] Assets Build Bundle Failed");
-        EditorApplication.Exit(1);
-      }
-    }
-  }
+        // https://docs.unity3d.com/ScriptReference/BuildPipeline.BuildPlayer.html
+        BuildReport buildReport = BuildPipeline.BuildPlayer(scenes, targetDir, buildTarget, buildOptions);
+        BuildSummary buildSummary = buildReport.summary;
+        if (buildSummary.result == BuildResult.Succeeded)
+        {
+            System.Console.WriteLine("[JenkinsBuild] Build Success: Time:" + buildSummary.totalTime + " Size:" + buildSummary.totalSize + " bytes");
+        }
+        else
+        {
+            System.Console.WriteLine("[JenkinsBuild] Build Failed: Time:" + buildSummary.totalTime + " Total Errors:" + buildSummary.totalErrors);
+            EditorApplication.Exit(1);
+        }
+        }
 }
---
+++ unity_cli.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+'/Applications/Unity Hub.app/Contents/MacOS/Unity Hub' -- --headless "$@"
